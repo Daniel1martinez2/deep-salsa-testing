57269cc914fab71e71c12830182f8f874c91d251
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-06-02 11:15:57 -0500

    better code

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
+from ast import Or
 import time
-from playsound import playsound
+from playsound import playsound as play_sound
 import threading
 
-VALUE = 377
+user_input = input('Which song (311, 369, 377, 388): ')
+VALUE =  int(user_input) if not user_input  == '' else 311
 SONG = f'./data/sound/tracks/{VALUE}.wav'
 DATA = f'./data/txtfiles/{VALUE}.txt'
-PULSE = './data/sound/pulse/bellShort.wav'
+PULSE = './data/sound/pulse/invaders.wav'
 
-def play1():
-    playsound(PULSE)
+def hit_pulse():
+    play_sound(PULSE)
+
+def play_song():
+    play_sound(SONG)
 
 threads = []
+t2 = threading.Thread(target=play_song)
 
 with open(DATA, 'r') as f:
     lines = f.readlines()
     lean = [int(x.replace(', bang;\n', '')) for x in lines]
     for i in lean:
         threads.append({
-            "th": threading.Thread(target=play1),
+            "th": threading.Thread(target=hit_pulse),
             "pulse": i
         })
 
-def play2():
-    playsound(SONG)
-
-t2 = threading.Thread(target=play2)
+# Start the song
 t2.start()
-
 counter = 0
+
 for thread in threads:
     time.sleep((thread['pulse'] - counter)/1000)
     counter = thread['pulse']
-    print(thread['pulse'])
+    print(f"{thread['pulse']} 💥💥💥")
     thread['th'].start()
